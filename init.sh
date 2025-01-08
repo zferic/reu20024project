096ba1ef65ef569dda02e7988c0c2b2bfc1800a0
zedeckj <zedeck.j@northeastern.edu>
2025-01-08 18:18:45 -0500

    init npm installs

--- init.sh
+++ init.sh
@@ -4,3 +4,7 @@ python3.10 -m venv venv
 source venv/bin/activate
 venv/bin/pip3 install -r requirements.txt
 
+npm install axios
+npm install react-icons
+npm install react-router-dom
+npm install react-markdown
